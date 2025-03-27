6f0d0774f03266f40db3c60d178a1b7ffe09cc3d
Viking690 <hoviktor2005@gmail.com>
2025-03-27 09:04:42 +0100

    Add files via upload

---
+++ style.js
@@ -0,0 +1,446 @@
+var Elektronika = localStorage.getItem("Elektronika");
+var ProgramozasAlapjai = localStorage.getItem("ProgramozasAlapjai");
+var Mikrovezerlo = localStorage.getItem("Mikrovezerlo");
+var DigitalisAramkorok = localStorage.getItem("DigitalisAramkorok");
+var AdatbAlapjai = localStorage.getItem("AdatbAlapjai");
+var Halozat = localStorage.getItem("Halozat");
+var PlcProgramozas = localStorage.getItem("PlcProgramozas");
+var SzamitogepesSzimulacio = localStorage.getItem("SzamitogepesSzimulacio");
+var Munkavallaloi = localStorage.getItem("Munkavallaloi");
+var IoT = localStorage.getItem("IoT");
+var Programfejlesztes = localStorage.getItem("Programfejlesztes");
+var Robottechnika = localStorage.getItem("Robottechnika ");
+
+
+
+function osztaly11()
+{
+    location.href = "tantargyak11.html";
+}
+function osztaly12()
+{
+    location.href = "tantargyak12.html";
+}
+function osztaly13()
+{
+    location.href = "tantargyak13.html";
+}
+function reflexio()
+{
+    location.href = "reflexio.html";
+}
+function elektronika()
+{
+    location.href = "elektronika.html";
+    var Elektronika = "notNull";
+    localStorage.setItem("Elektronika", JSON.stringify(Elektronika));
+    localStorage.removeItem("ProgramozasAlapjai");
+    localStorage.removeItem("Mikrovezerlo");
+    localStorage.removeItem("DigitalisAramkorok");
+    localStorage.removeItem("AdatbAlapjai");
+    localStorage.removeItem("Halozat");
+    localStorage.removeItem("PlcProgramozas");
+    localStorage.removeItem("SzamitogepesSzimulacio");
+    localStorage.removeItem("Munkavallaloi");
+    localStorage.removeItem("IoT");
+    localStorage.removeItem("Programfejlesztes");
+    localStorage.removeItem("Robottechnika");
+}
+function programozasAlapjai()
+{
+    location.href = "programozas.html";
+    var ProgramozasAlapjai = "notNull";
+    localStorage.setItem("ProgramozasAlapjai", JSON.stringify(ProgramozasAlapjai));
+    localStorage.removeItem("Elektronika");
+    localStorage.removeItem("Mikrovezerlo");
+    localStorage.removeItem("DigitalisAramkorok");
+    localStorage.removeItem("AdatbAlapjai");
+    localStorage.removeItem("Halozat");
+    localStorage.removeItem("PlcProgramozas");
+    localStorage.removeItem("SzamitogepesSzimulacio");
+    localStorage.removeItem("Munkavallaloi");
+    localStorage.removeItem("IoT");
+    localStorage.removeItem("Programfejlesztes");
+    localStorage.removeItem("Robottechnika");
+    
+}
+
+function mikrovezerlo()
+{
+    location.href = "mikrovezerlo.html";
+    var Mikrovezerlo = "notNull";
+    localStorage.setItem("Mikrovezerlo", JSON.stringify(Mikrovezerlo));
+    localStorage.removeItem("Elektronika");
+    localStorage.removeItem("ProgramozasAlapjai");
+    localStorage.removeItem("DigitalisAramkorok");
+    localStorage.removeItem("AdatbAlapjai");
+    localStorage.removeItem("Halozat");
+    localStorage.removeItem("PlcProgramozas");
+    localStorage.removeItem("SzamitogepesSzimulacio");
+    localStorage.removeItem("Munkavallaloi");
+    localStorage.removeItem("IoT");
+    localStorage.removeItem("Programfejlesztes");
+    localStorage.removeItem("Robottechnika");
+    
+}
+function digitalisAramkorok()
+{
+    location.href = "digitalis.html";
+    var DigitalisAramkorok = "notNull";
+    localStorage.setItem("DigitalisAramkorok", JSON.stringify(DigitalisAramkorok));
+    localStorage.removeItem("Elektronika");
+    localStorage.removeItem("ProgramozasAlapjai");
+    localStorage.removeItem("Mikrovezerlo");
+    localStorage.removeItem("AdatbAlapjai");
+    localStorage.removeItem("Halozat");
+    localStorage.removeItem("PlcProgramozas");
+    localStorage.removeItem("SzamitogepesSzimulacio");
+    localStorage.removeItem("Munkavallaloi");
+    localStorage.removeItem("IoT");
+    localStorage.removeItem("Programfejlesztes");
+    localStorage.removeItem("Robottechnika");
+    
+}
+function adatbAlapjai()
+{
+    location.href = "adatbazis.html";
+    var AdatbAlapjai = "notNull";
+    localStorage.setItem("AdatbAlapjai", JSON.stringify(AdatbAlapjai));
+    localStorage.removeItem("Elektronika");
+    localStorage.removeItem("ProgramozasAlapjai");
+    localStorage.removeItem("Mikrovezerlo");
+    localStorage.removeItem("DigitalisAramkorok");
+    localStorage.removeItem("Halozat");
+    localStorage.removeItem("PlcProgramozas");
+    localStorage.removeItem("SzamitogepesSzimulacio");
+    localStorage.removeItem("Munkavallaloi");
+    localStorage.removeItem("IoT");
+    localStorage.removeItem("Programfejlesztes");
+    localStorage.removeItem("Robottechnika");
+}
+function halozat()
+{
+    location.href = "halozat.html";
+    var Halozat = "notNull";
+    localStorage.setItem("Halozat", JSON.stringify(Halozat));
+    localStorage.removeItem("Elektronika");
+    localStorage.removeItem("ProgramozasAlapjai");
+    localStorage.removeItem("Mikrovezerlo");
+    localStorage.removeItem("DigitalisAramkorok");
+    localStorage.removeItem("AdatbAlapjai");
+    localStorage.removeItem("PlcProgramozas");
+    localStorage.removeItem("SzamitogepesSzimulacio");
+    localStorage.removeItem("Munkavallaloi");
+    localStorage.removeItem("IoT");
+    localStorage.removeItem("Programfejlesztes");
+    localStorage.removeItem("Robottechnika");
+    
+}
+function plcProgramozas()
+{
+    location.href = "plc.html";
+    var PlcProgramozas = "notNull";
+    localStorage.setItem("PlcProgramozas", JSON.stringify(PlcProgramozas));
+    localStorage.removeItem("Elektronika");
+    localStorage.removeItem("ProgramozasAlapjai");
+    localStorage.removeItem("Mikrovezerlo");
+    localStorage.removeItem("DigitalisAramkorok");
+    localStorage.removeItem("AdatbAlapjai");
+    localStorage.removeItem("Halozat");
+    localStorage.removeItem("SzamitogepesSzimulacio");
+    localStorage.removeItem("Munkavallaloi");
+    localStorage.removeItem("IoT");
+    localStorage.removeItem("Programfejlesztes");
+    localStorage.removeItem("Robottechnika");
+}
+function szamitogepesSzimulacio()
+{
+    location.href = "szamitogepes.html";
+    var SzamitogepesSzimulacio = "notNull";
+    localStorage.setItem("SzamitogepesSzimulacio", JSON.stringify(SzamitogepesSzimulacio));
+    localStorage.removeItem("Elektronika");
+    localStorage.removeItem("ProgramozasAlapjai");
+    localStorage.removeItem("Mikrovezerlo");
+    localStorage.removeItem("DigitalisAramkorok");
+    localStorage.removeItem("AdatbAlapjai");
+    localStorage.removeItem("Halozat");
+    localStorage.removeItem("PlcProgramozas");
+    localStorage.removeItem("Munkavallaloi");
+    localStorage.removeItem("IoT");
+    localStorage.removeItem("Programfejlesztes");
+    localStorage.removeItem("Robottechnika");
+}
+function munkavallaloi()
+{
+    location.href = "munkavallaloi.html";
+    var Munkavallaloi = "notNull";
+    localStorage.setItem("Munkavallaloi", JSON.stringify(Munkavallaloi));
+    localStorage.removeItem("Elektronika");
+    localStorage.removeItem("ProgramozasAlapjai");
+    localStorage.removeItem("Mikrovezerlo");
+    localStorage.removeItem("DigitalisAramkorok");
+    localStorage.removeItem("AdatbAlapjai");
+    localStorage.removeItem("Halozat");
+    localStorage.removeItem("PlcProgramozas");
+    localStorage.removeItem("SzamitogepesSzimulacio");
+    localStorage.removeItem("IoT");
+    localStorage.removeItem("Programfejlesztes");
+    localStorage.removeItem("Robottechnika");
+}
+function ioT()
+{
+    location.href = "Iot.html";
+    var IoT = "notNull";
+    localStorage.setItem("IoT", JSON.stringify(IoT));
+    localStorage.removeItem("Elektronika");
+    localStorage.removeItem("ProgramozasAlapjai");
+    localStorage.removeItem("Mikrovezerlo");
+    localStorage.removeItem("DigitalisAramkorok");
+    localStorage.removeItem("AdatbAlapjai");
+    localStorage.removeItem("Halozat");
+    localStorage.removeItem("PlcProgramozas");
+    localStorage.removeItem("SzamitogepesSzimulacio");
+    localStorage.removeItem("Munkavallaloi");
+    localStorage.removeItem("Programfejlesztes");
+    localStorage.removeItem("Robottechnika");
+}
+function programfejlesztes()
+{
+    location.href = "profe.html";
+    var programfejlesztes = "notNull";
+    localStorage.setItem("Programfejlesztes", JSON.stringify(Programfejlesztes));
+    localStorage.removeItem("Elektronika");
+    localStorage.removeItem("ProgramozasAlapjai");
+    localStorage.removeItem("Mikrovezerlo");
+    localStorage.removeItem("DigitalisAramkorok");
+    localStorage.removeItem("AdatbAlapjai");
+    localStorage.removeItem("Halozat");
+    localStorage.removeItem("PlcProgramozas");
+    localStorage.removeItem("SzamitogepesSzimulacio");
+    localStorage.removeItem("Munkavallaloi");
+    localStorage.removeItem("IoT");
+    localStorage.removeItem("Robottechnika");
+    
+}
+function robottechnika()
+{
+    location.href = "robot.html";
+    var Robottechnika = "notNull";
+    localStorage.setItem("Robottechnika", JSON.stringify(Robottechnika));
+    localStorage.removeItem("Elektronika");
+    localStorage.removeItem("ProgramozasAlapjai");
+    localStorage.removeItem("Mikrovezerlo");
+    localStorage.removeItem("DigitalisAramkorok");
+    localStorage.removeItem("AdatbAlapjai");
+    localStorage.removeItem("Halozat");
+    localStorage.removeItem("PlcProgramozas");
+    localStorage.removeItem("SzamitogepesSzimulacio");
+    localStorage.removeItem("Munkavallaloi");
+    localStorage.removeItem("IoT");
+    localStorage.removeItem("Programfejlesztes");
+}
+if (Elektronika != null) 
+{
+    var img1 = document.getElementById('img1');
+    img1.src = "2.png";
+    var img1 = document.getElementById('img2');
+    img2.src = "1.png";
+    var img1 = document.getElementById('img3');
+    img3.src = "3.jpg";
+    var img1 = document.getElementById('img4');
+    img4.src = "3.jpg";
+    var img1 = document.getElementById('img5');
+    img5.src = "1.png";
+    var img1 = document.getElementById('img6');
+    img6.src = "2.png";
+    var video = document.getElementById("video");
+    video.src = "video.mp4";
+}
+else if (ProgramozasAlapjai != null) 
+    {
+    var img1 = document.getElementById('img1');
+    img1.src = "1.png";
+    var img1 = document.getElementById('img2');
+    img2.src = "2.png";
+    var img1 = document.getElementById('img3');
+    img3.src = "3.jpg";
+    var img1 = document.getElementById('img4');
+    img4.src = "2.png";
+    var img1 = document.getElementById('img5');
+    img5.src = "1.png";
+    var img1 = document.getElementById('img6');
+    img6.src = "2.png";
+    var video = document.getElementById("video");
+    video.src = "";
+}
+else if (Mikrovezerlo != null) 
+    {
+    var img1 = document.getElementById('img1');
+    img1.src = "2.png";
+    var img1 = document.getElementById('img2');
+    img2.src = "1.png";
+    var img1 = document.getElementById('img3');
+    img3.src = "3.jpg";
+    var img1 = document.getElementById('img4');
+    img4.src = "1.png";
+    var img1 = document.getElementById('img5');
+    img5.src = "2.png";
+    var img1 = document.getElementById('img6');
+    img6.src = "2.png";
+    var video = document.getElementById("video");
+    video.src = "";
+}
+else if (DigitalisAramkorok != null) 
+    {
+    var img1 = document.getElementById('img1');
+    img1.src = "1.png";
+    var img1 = document.getElementById('img2');
+    img2.src = "2.png";
+    var img1 = document.getElementById('img3');
+    img3.src = "3.jpg";
+    var img1 = document.getElementById('img4');
+    img4.src = "2.png";
+    var img1 = document.getElementById('img5');
+    img5.src = "1.png";
+    var img1 = document.getElementById('img6');
+    img6.src = "2.png";
+    var video = document.getElementById("video");
+    video.src = "";
+}
+else if (AdatbAlapjai != null) 
+    {
+    var img1 = document.getElementById('img1');
+    img1.src = "1.png";
+    var img1 = document.getElementById('img2');
+    img2.src = "2.png";
+    var img1 = document.getElementById('img3');
+    img3.src = "3.jpg";
+    var img1 = document.getElementById('img4');
+    img4.src = "2.png";
+    var img1 = document.getElementById('img5');
+    img5.src = "1.png";
+    var img1 = document.getElementById('img6');
+    img6.src = "2.png";
+    var video = document.getElementById("video");
+    video.src = "";
+}
+else if (Halozat != null) 
+    {
+    var img1 = document.getElementById('img1');
+    img1.src = "1.png";
+    var img1 = document.getElementById('img2');
+    img2.src = "2.png";
+    var img1 = document.getElementById('img3');
+    img3.src = "3.jpg";
+    var img1 = document.getElementById('img4');
+    img4.src = "2.png";
+    var img1 = document.getElementById('img5');
+    img5.src = "1.png";
+    var img1 = document.getElementById('img6');
+    img6.src = "2.png";
+    var video = document.getElementById("video");
+    video.src = "";
+}
+else if (PlcProgramozas != null) 
+    {
+    var img1 = document.getElementById('img1');
+    img1.src = "1.png";
+    var img1 = document.getElementById('img2');
+    img2.src = "2.png";
+    var img1 = document.getElementById('img3');
+    img3.src = "3.jpg";
+    var img1 = document.getElementById('img4');
+    img4.src = "2.png";
+    var img1 = document.getElementById('img5');
+    img5.src = "1.png";
+    var img1 = document.getElementById('img6');
+    img6.src = "2.png";
+    var video = document.getElementById("video");
+    video.src = "";
+}
+else if (SzamitogepesSzimulacio != null) 
+    {
+    var img1 = document.getElementById('img1');
+    img1.src = "1.png";
+    var img1 = document.getElementById('img2');
+    img2.src = "2.png";
+    var img1 = document.getElementById('img3');
+    img3.src = "3.jpg";
+    var img1 = document.getElementById('img4');
+    img4.src = "2.png";
+    var img1 = document.getElementById('img5');
+    img5.src = "1.png";
+    var img1 = document.getElementById('img6');
+    img6.src = "2.png";
+    var video = document.getElementById("video");
+    video.src = "";
+}
+else if (Munkavallaloi != null) 
+    {
+    var img1 = document.getElementById('img1');
+    img1.src = "1.png";
+    var img1 = document.getElementById('img2');
+    img2.src = "2.png";
+    var img1 = document.getElementById('img3');
+    img3.src = "3.jpg";
+    var img1 = document.getElementById('img4');
+    img4.src = "2.png";
+    var img1 = document.getElementById('img5');
+    img5.src = "1.png";
+    var img1 = document.getElementById('img6');
+    img6.src = "2.png";
+    var video = document.getElementById("video");
+    video.src = "";
+}
+
+else if (IoT!= null) 
+    {
+    var img1 = document.getElementById('img1');
+    img1.src = "1.png";
+    var img1 = document.getElementById('img2');
+    img2.src = "2.png";
+    var img1 = document.getElementById('img3');
+    img3.src = "3.jpg";
+    var img1 = document.getElementById('img4');
+    img4.src = "2.png";
+    var img1 = document.getElementById('img5');
+    img5.src = "1.png";
+    var img1 = document.getElementById('img6');
+    img6.src = "2.png";
+    var video = document.getElementById("video");
+    video.src = "";
+}
+else if (Programfejlesztes != null) 
+    {
+    var img1 = document.getElementById('img1');
+    img1.src = "1.png";
+    var img1 = document.getElementById('img2');
+    img2.src = "2.png";
+    var img1 = document.getElementById('img3');
+    img3.src = "3.jpg";
+    var img1 = document.getElementById('img4');
+    img4.src = "2.png";
+    var img1 = document.getElementById('img5');
+    img5.src = "1.png";
+    var img1 = document.getElementById('img6');
+    img6.src = "2.png";
+    var video = document.getElementById("video");
+    video.src = "";
+}
+else
+{
+    var img1 = document.getElementById('img1');
+    img1.src = "1.png";
+    var img1 = document.getElementById('img2');
+    img2.src = "2.png";
+    var img1 = document.getElementById('img3');
+    img3.src = "3.jpg";
+    var img1 = document.getElementById('img4');
+    img4.src = "2.png";
+    var img1 = document.getElementById('img5');
+    img5.src = "1.png";
+    var img1 = document.getElementById('img6');
+    img6.src = "2.png";
+    var video = document.getElementById("video");
+    video.src = "";
+}
